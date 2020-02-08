a68c0086f67d99482b003a26d87030367170cb9f
Serg Chernata <sergchernata@gmail.com>
2020-02-08 08:14:12 -0500

    refactored path definitions and added graceful remnode shutdown

--- restoresnapshot.sh
+++ restoresnapshot.sh
@@ -31,20 +31,39 @@
 # █████   █████░░██████  ██████   ░░█████ ░░██████  █████    ░░██████  █████                 #
 #░░░░░   ░░░░░  ░░░░░░  ░░░░░░     ░░░░░   ░░░░░░  ░░░░░      ░░░░░░  ░░░░░                  #
 
-lastdownloadfolder=./data/snapshots/lastdownload/
-mkdir -p $lastdownloadfolder
-cd /root/data/snapshots
-mkdir -p lastdownload
-cd lastdownload
+# path definitions
+datafolder=/root/data
+blocksfolder=$datafolder/blocks
+statefolder=$datafolder/state
+snapshotsfolder=$datafolder/snapshots
+lastdownloadfolder=$snapshotsfolder/lastdownload
 
+# create download folder in snapshots
+mkdir -p $lastdownloadfolder
+cd $lastdownloadfolder
+
+#download the latest snapshot
 latestsnapshot=$(curl -s https://geordier.co.uk/snapshots/latestsnapshot.php)
 wget -c https://www.geordier.co.uk/snapshots/$latestsnapshot -O - | sudo tar -xz --strip=4
-killall -q remnode
-sleep 2
 binfile=$lastdownloadfolder*.bin
-cd ../../
-rm -rf ./blocks*/
-rm -rf ./state
+
+# gracefully stop remnode
+remnode_pid=$(pgrep remnode)
+
+if [ ! -z "$remnode_pid" ]; then
+    if ps -p $remnode_pid > /dev/null; then
+        kill -SIGINT $remnode_pid
+    fi
+    while ps -p $remnode_pid > /dev/null; do
+        sleep 1
+    done
+fi
+
+# clean up old data before snapshot sync
+rm -rf $blocksfolder*/
+rm -rf $statefolder
+
+# start remnode with snapshot
 cd ~
 remnode --config-dir ./config/ --snapshot $(ls -t $binfile | head -n1) --data-dir ./data/ >> remnode.log 2>&1 &
 sleep 3
