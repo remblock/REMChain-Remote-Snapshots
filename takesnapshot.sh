2dc238cc76a60edd14293858cf7fb206edc44f90
Geordie-R <40744776+Geordie-R@users.noreply.github.com>
2020-02-13 00:55:27 +0000

    Update takesnapshot.sh
    Only keep the last 5 cmpressed snapshots in the compressed folder

--- takesnapshot.sh
+++ takesnapshot.sh
@@ -1,4 +1,5 @@
 #!/bin/bash
+###### Parameter Section #####
 sshportno=466
 datename=$(date +%Y-%m-%d)
 shcreate=compressandsendlastsnapshot.sh
@@ -11,6 +12,10 @@ snapname=$(curl http://127.0.0.1:8888/v1/producer/create_snapshot | jq '.snapsho
 
 rm -f $shcreate
 touch $shcreate && chmod +x $shcreate
+
+#List all .gz files especially the last 5 by filename aand remove the rest
+ls -F *.gz | head -n -5 | xargs -r rm
+
 echo "tar -cvzf $filename $snapname" >> $shcreate
 echo "rsync -rv -e 'ssh -p $sshportno' --progress $filename root@website.geordier.co.uk:/var/www/geordier.co.uk/snapshots" >> $shcreate
 ./$shcreate
